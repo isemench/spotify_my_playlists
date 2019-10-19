69076e371ea439db9d9824b9d1614bfef7a06e1e
Igor Semenchukov <igor.semenchukov@gmail.com>
2019-10-19 17:48:50 +0300

    Add Spotify bare authorization code.
    Code is able to send authorization request for the Spotify application.
    Spotify IDs shall be provided separately and shall correspond to the IDs
    of the existing Spotify application.

---
+++ log.js
@@ -0,0 +1,13 @@
+var log = {
+  info: function(msg) {
+    console.log("Info: " + msg);
+  },
+  warning: function(msg) {
+    console.log("Warning: " + msg);
+  },
+  error: function(msg) {
+    console.log("Error: " + msg);
+  }
+};
+
+module.exports = log
